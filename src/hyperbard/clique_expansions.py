672bde5f643041c3914faa88bb151546f8eba144
dataspider <corinna.coupette@gmail.com>
2022-05-25 11:29:35 +0200

    star expansions WIP check-in (currently also in clique_expansions, and desparately needing a separate plot, too)

--- src/hyperbard/clique_expansions.py
+++ src/hyperbard/clique_expansions.py
@@ -48,6 +48,63 @@ def get_weighted_multigraph(df: pd.DataFrame, groupby: list):
     return mG
 
 
+def get_bipartite_graph(df: pd.DataFrame, groupby: list):
+    """
+    groupby = ["act", "scene"] -> one play part node per act and scene
+    groupby = ["act", "scene", "stagegroup"] -> one play part node per act, scene, and stagegroup
+    groupby = ["act", "scene", "stagegroup", "setting", "speaker"] ->
+    n_tokens or n_lines are potential weights
+    """
+    agg = dict(onstage=join_strings, n_tokens=sum, n_lines=sum)
+    df_aggregated = df.groupby(groupby).agg(agg).reset_index()
+    df_aggregated["onstage"] = df_aggregated["onstage"].map(
+        character_string_to_sorted_list
+    )
+    if groupby != ["act", "scene", "stagegroup", "setting", "speaker"]:
+        G = nx.Graph()
+        text_units = list(
+            zip(*[df_aggregated[c].values for c in df_aggregated[groupby].columns])
+        )
+        G.add_nodes_from(
+            [elem for sublist in df_aggregated.onstage for elem in sublist],
+            node_type="character",
+        )
+        G.add_nodes_from(text_units, node_type="text_unit")
+        for idx, row in df_aggregated.iterrows():
+            row_node = tuple(row[x] for x in groupby)
+            G.add_edges_from(
+                [(row_node, character) for character in row["onstage"]],
+                n_lines=row["n_lines"],
+                n_tokens=row["n_tokens"],
+            )
+    else:
+        G = nx.MultiDiGraph()
+        text_units = list(
+            zip(*[df_aggregated[c].values for c in df_aggregated[groupby[:3]].columns])
+        )
+        G.add_nodes_from(
+            [elem for sublist in df_aggregated.onstage for elem in sublist],
+            node_type="character",
+        )
+        G.add_nodes_from(text_units, node_type="text_unit")
+        for idx, row in df_aggregated.iterrows():
+            row_node = tuple(row[x] for x in groupby[:3])
+            row_speaker = row["speaker"]
+            row_lines = row["n_lines"]
+            row_tokens = row["n_tokens"]
+            G.add_edge(row_speaker, row_node, n_lines=row_lines, n_tokens=row_tokens)
+            G.add_edges_from(
+                [
+                    (row_node, character)
+                    for character in row["onstage"]
+                    if character != row_speaker
+                ],
+                n_lines=row_lines,
+                n_tokens=row_tokens,
+            )
+    return G
+
+
 def get_count_weighted_graph(df: pd.DataFrame, groupby: list):
     """
     groupby = ["act", "scene"] -> one edge per act and scene
@@ -64,34 +121,71 @@ def get_count_weighted_graph(df: pd.DataFrame, groupby: list):
     return G
 
 
-def degree_centrality(G, weight=None):
+def degree_centrality(G, weight=None, centrality=None):
     """
+    centrality: None or "in" or "out"
     Wrapper around nx.degree_centrality that allows to account for weights.
     When weight attribute is specified, returns fraction of degree sum in which node participates.
     """
-    if weight is None:
-        return nx.degree_centrality(G)
-    else:
+    # Defenses against garbage input
+    if centrality not in [None, "in", "out"]:
+        raise ValueError(f"centrality={centrality}, must be in {[None, 'in', 'out']}!")
+    if centrality is not None and type(G) not in [nx.DiGraph, nx.MultiDiGraph]:
+        raise ValueError(
+            f"type(G)={type(G)}, must be in {[nx.DiGraph, nx.MultiDiGraph]} because centrality={centrality}!"
+        )
+    # Actual centrality computation
+    if weight is not None:
         assert (
             weight in list(G.edges(data=True))[0][-1].keys()
         ), f"Attribute '{weight}'\
-        is not an edge attribute! Edge attributes are: {list(list(G.edges(data=True))[0][-1].keys())}"
-        s = 1.0 / sum(dict(G.degree(weight=weight)).values())
-        centrality = {n: d * s for n, d in G.degree(weight=weight)}
-        return centrality
+            is not an edge attribute! Edge attributes are: {list(list(G.edges(data=True))[0][-1].keys())}"
+    if "node_type" not in list(dict(G.nodes(data=True)).values())[0].keys():
+        if weight is None:
+            centrality = nx.degree_centrality(G)
+        else:
+            s = 1.0 / sum(dict(G.degree(weight=weight)).values())
+            centrality = {n: d * s for n, d in G.degree(weight=weight)}
+    else:
+        character_nodes = [
+            n for n, node_type in G.nodes(data="node_type") if node_type == "character"
+        ]
+        if type(G) == nx.Graph:
+            s = 1.0 / sum(dict(G.degree(character_nodes, weight=weight)).values())
+            centrality = {n: d * s for n, d in G.degree(character_nodes, weight=weight)}
+        elif type(G) == nx.MultiDiGraph:
+            if centrality == "out":
+                degree_func = G.out_degree
+            elif centrality == "in":
+                degree_func = G.in_degree
+            else:
+                degree_func = G.degree
+            s = 1.0 / sum(dict(degree_func(character_nodes, weight=weight)).values())
+            centrality = {
+                n: d * s for n, d in degree_func(character_nodes, weight=weight)
+            }
+        else:
+            raise NotImplementedError(f"Unexpected graph type: {type(G)}!")
+    return centrality
 
 
-def centrality_ranking(G, weight=None):
+def centrality_ranking(G, weight=None, centrality=None):
+    """
+    centrality: None or "in" or "out"
+    """
     return sorted(
-        degree_centrality(G, weight).items(), key=lambda tup: tup[-1], reverse=True
+        degree_centrality(G, weight, centrality).items(),
+        key=lambda tup: tup[-1],
+        reverse=True,
     )
 
 
-def centrality_ranking_with_equalities(G, weight=None):
+def centrality_ranking_with_equalities(G, weight=None, centrality=None):
     """
+    centrality: None or "in" or "out"
     output: list of tuples [({set of characters}, centrality), ...], sorted by centrality descending
     """
-    ranking_list = centrality_ranking(G, weight)
+    ranking_list = centrality_ranking(G, weight, centrality)
     new_list = []
     for character, centrality in ranking_list:
         if new_list and centrality == new_list[-1][-1]:
@@ -116,7 +210,33 @@ def get_character_ranking_df(df):
     mG2 = get_weighted_multigraph(df, groupby=["act", "scene", "stagegroup"])
     G = get_count_weighted_graph(df, groupby=["act", "scene"])
     G2 = get_count_weighted_graph(df, groupby=["act", "scene", "stagegroup"])
+    bG = get_bipartite_graph(df, groupby=["act", "scene"])
+    bG2 = get_bipartite_graph(df, groupby=["act", "scene", "stagegroup"])
+    bG3 = get_bipartite_graph(
+        df, groupby=["act", "scene", "stagegroup", "setting", "speaker"]
+    )
     ranks = {
+        "001_act_scene_bipartite": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG)
+        ),
+        "002_act_scene_bipartite_lines": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG, weight="n_lines")
+        ),
+        "003_act_scene_stagegroup_bipartite": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG2)
+        ),
+        "004_act_scene_stagegroup_bipartite_lines": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG2, weight="n_lines")
+        ),
+        "005_act_scene_stagegroup_setting_bipartite_lines_in": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG3, weight="n_lines", centrality="in")
+        ),
+        "006_act_scene_stagegroup_setting_bipartite_lines_out": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG3, weight="n_lines", centrality="out")
+        ),
+        "007_act_scene_stagegroup_setting_bipartite_lines_io": character_rank_dictionary(
+            centrality_ranking_with_equalities(bG3, weight="n_lines", centrality=None)
+        ),
         "01_act_scene_simple": character_rank_dictionary(
             centrality_ranking_with_equalities(G)
         ),
