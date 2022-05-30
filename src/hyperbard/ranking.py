c53ad91fd13e0280d9eb68e6d2f47a5da4001c45
Bastian Rieck <bastian@rieck.me>
2022-05-30 10:17:49 +0200

    More cleaning of raking script
    Removed obsolete plot for comparing different centrality measures. Will
    check calculation of degree calculations next.

--- src/hyperbard/ranking.py
+++ src/hyperbard/ranking.py
@@ -1,21 +1,12 @@
 """Simple summary statistics for hypergraphs."""
 
-import argparse
 import collections
 
 import hypernetx as hnx
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import seaborn as sns
 
-from hypernetx.algorithms.s_centrality_measures import (
-    s_betweenness_centrality,
-    s_closeness_centrality,
-    s_eccentricity,
-    s_harmonic_centrality,
-)
 
 from hyperbard.representations import (
     get_bipartite_graph,
@@ -31,66 +22,6 @@ def s_degree_centrality(H, s=1, **kwargs):
     for node in H.nodes:
         values[node] = H.s_degree(node, s=s)
     return values
-
-
-def calculate_centrality(hypergraphs, normalise=True):
-    """Calculate centrality for all nodes.
-
-    This function accumulates all centrality values over different
-    s-connectivity thresholds and stores them for each node.
-
-    Parameters
-    ----------
-    normalise : bool
-        If set, normalise centralities to sum to one, thus counting the
-        overall contributions to centrality for each node.
-
-    Returns
-    -------
-    pd.DataFrame
-        Data frame containing node names (i.e. characters) as its index,
-        and different centrality measures as columns.
-    """
-    # Key will be the name of a centrality measure; the value will be
-    # the standard counter
-    centrality = collections.defaultdict(collections.Counter)
-
-    # Auxiliary function for normalising a dictionary (or really any
-    # other type of key--value store).
-    def _normalise(d):
-        factor = sum(d.values())
-        if factor > 0:
-            return {k: v / factor for k, v in d.items()}
-        else:
-            return d
-
-    centrality_functions = {
-        "betweenness_centrality": s_betweenness_centrality,
-        "closeness_centrality": s_closeness_centrality,
-        "degree_centrality": s_degree_centrality,
-        "eccentricity": s_eccentricity,
-        "harmonic_centrality": s_harmonic_centrality,
-    }
-
-    for k, v in hypergraphs.items():
-        # TODO: go deeper/higher? I have not yet found a way to query
-        # the hypergraph about its maximum $s$-connectivity.
-        for s in [1, 2, 5]:
-            for name, fn in centrality_functions.items():
-                values = fn(v, s=s, edges=False)
-
-                if normalise:
-                    values = _normalise(values)
-
-                centrality[name].update(values)
-
-    columns = [
-        pd.Series(values, name=name).sort_index() for name, values in centrality.items()
-    ]
-
-    df = pd.concat(columns, axis="columns")
-    df.index.name = "character"
-    return df
 
 
 def degree_centrality(G, weight=None, centrality=None):
