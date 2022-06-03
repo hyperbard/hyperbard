df83182d61c551db91fc97e7517903e17f90b4e5
Bastian Rieck <bastian@rieck.me>
2022-06-03 09:52:38 +0200

    Using aggregated data as well

--- src/hyperbard/summary_statistics_raw.py
+++ src/hyperbard/summary_statistics_raw.py
@@ -7,25 +7,30 @@ from statics import DATA_PATH
 import pandas as pd
 
 
-
 if __name__ == '__main__':
-    filenames = sorted(glob(f"{DATA_PATH}/*.raw.csv"))
+    filenames_raw = sorted(glob(f"{DATA_PATH}/*.raw.csv"))
+    filenames_agg = sorted(glob(f"{DATA_PATH}/*.agg.csv"))
 
     # Will contain the results of all operations of this script.
     df_out = []
 
-    for filename in filenames:
-        df = pd.read_csv(filename, low_memory=False)
-        play = get_filename_base(filename)
+    for filename_raw, filename_agg in zip(filenames_raw, filenames_agg):
+        df_raw = pd.read_csv(filename_raw, low_memory=False)
+        df_agg = pd.read_csv(filename_agg, low_memory=False)
+
+        play = get_filename_base(filename_raw)
         play = play.split('_')[0]
 
-        n_characters = len(df['speaker'].dropna().unique())
-        n_words = len(df[df['tag'] == 'w'])
+        n_characters = len(df_raw['speaker'].dropna().unique())
+
+        n_words = df_agg['n_tokens'].sum()
+        n_lines = df_agg['n_lines'].sum()
 
         row = {
             'play': play,
             'n_characters': n_characters,
             'n_words': n_words,
+            'n_lines': n_lines,
         }
 
         df_out.append(row)
