b194e3ae1d7b988a805fcf124f2fedb29c004227
dataspider <corinna.coupette@gmail.com>
2022-06-05 16:45:38 +0200

    determine number of characters based on castList

--- hyperbard/summary_statistics_raw.py
+++ hyperbard/summary_statistics_raw.py
@@ -9,27 +9,17 @@ from hyperbard.utils import get_filename_base
 
 
 def compute_raw_statistics(
-    filename_raw: str, filename_agg: str, name_to_type: pd.DataFrame
+    filename_cast: str, filename_agg: str, name_to_type: pd.DataFrame
 ) -> dict:
-    play = get_filename_base(filename_raw, full=False)
-    assert play == get_filename_base(
-        filename_agg, full=False
-    ), f"Filenames don't match - raw: {filename_raw}, agg: {filename_agg} - check your data!"
+    play = get_filename_base(filename_cast, full=True).split(".")[0]
+    assert (
+        play == get_filename_base(filename_agg, full=True).split(".")[0]
+    ), f"Filenames don't match - raw: {filename_cast}, agg: {filename_agg} - check your data!"
 
-    df_raw = pd.read_csv(filename_raw, low_memory=False)
+    df_cast = pd.read_csv(filename_cast, low_memory=False)
     df_agg = pd.read_csv(filename_agg, low_memory=False)
 
-    # TODO fix
-    # this currently counts unique characters that speak ("active characters")
-    # - undercounting: characters that do not speak
-    # fix could look at other part of raw data (currently thrown away)
-    speaking_characters = {
-        item
-        for sublist in df_raw.speaker.dropna().map(lambda x: x.split())
-        for item in sublist
-    }
-    n_characters = len(speaking_characters)
-
+    n_characters = len(df_cast)
     n_words = df_agg["n_tokens"].sum()
     n_lines = df_agg["n_lines"].sum()
 
@@ -54,9 +44,9 @@ def compute_all_raw_statistics(filenames_raw, filenames_agg, name_to_type):
 
 
 if __name__ == "__main__":
-    filenames_raw = sorted(glob(f"{DATA_PATH}/*.raw.csv"))
+    filenames_cast = sorted(glob(f"{DATA_PATH}/*.cast.csv"))
     filenames_agg = sorted(glob(f"{DATA_PATH}/*.agg.csv"))
     name_to_type = pd.read_csv(f"{META_PATH}/playtypes.csv").set_index("play_name")
-    df = compute_all_raw_statistics(filenames_raw, filenames_agg, name_to_type)
+    df = compute_all_raw_statistics(filenames_cast, filenames_agg, name_to_type)
     df.to_csv(f"{META_PATH}/summary_statistics_raw.csv", index=False)
-    # print(df.to_csv(index=False))
+    #  print(df.to_csv(index=False))
