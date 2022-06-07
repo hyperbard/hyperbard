0e619aabb6f5fdc8fca3dced436542a93387ccff
Bastian Rieck <bastian@rieck.me>
2022-06-07 18:39:41 +0200

    Prevent pre-processing from overwriting files

--- src/hyperbard/run_preprocessing.py
+++ src/hyperbard/run_preprocessing.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from glob import glob
 from multiprocessing import Pool, cpu_count
@@ -12,18 +13,38 @@ def handle_file(file):
     try:
         print(f"Starting {file}...")
         filename_base = get_filename_base(file, full=False)
-        #  .cast.csv
-        cast_df = get_cast_df(file)
-        cast_df.to_csv(f"{DATA_PATH}/{filename_base}.cast.csv", index=False)
-        #  .raw.csv
-        df = get_raw_xml_df(file)
-        df.to_csv(f"{DATA_PATH}/{filename_base}.raw.csv", index=False)
-        # .agg.csv
-        aggdf = get_agg_xml_df(df)
-        assert all(
-            [bool(x) for x in aggdf.onstage]
-        ), f"{file}: found nan values in 'onstage' column of aggregated (i.e., speech-only) dataframe!"
-        aggdf.to_csv(f"{DATA_PATH}/{filename_base}.agg.csv", index=False)
+
+        out_file = f"{DATA_PATH}/{filename_base}.cast.csv"
+
+        if os.path.exists(out_file) and not args.force:
+            print(f"{out_file} already exists; will not overwrite")
+        else:
+            #  .cast.csv
+            cast_df = get_cast_df(file)
+            cast_df.to_csv(f"{out_file}", index=False)
+
+        out_file = f"{DATA_PATH}/{filename_base}.raw.csv"
+
+        if os.path.exists(out_file) and not args.force:
+            print(f"{out_file} already exists; will not overwrite")
+        else:
+            #  .raw.csv
+            df = get_raw_xml_df(file)
+            df.to_csv(f"{out_file}", index=False)
+
+        out_file = f"{DATA_PATH}/{filename_base}.agg.csv"
+
+        if os.path.exists(out_file) and not args.force:
+            print(f"{out_file} already exists; will not overwrite")
+        else:
+            aggdf = get_agg_xml_df(df)
+            assert all(
+                [bool(x) for x in aggdf.onstage]
+            ), f"{file}: found nan values in 'onstage' column of aggregated (i.e., speech-only) dataframe!"
+
+            # .agg.csv
+            aggdf.to_csv(f"{out_file}", index=False)
+
     except TypeError as e:
         raise Exception(f"Problem with {file}: {e}")
 
@@ -33,7 +54,14 @@ if __name__ == "__main__":
     print(f"Found {len(files)} files to process.")
     os.makedirs(DATA_PATH, exist_ok=True)
 
-    # for file in files:
-    #     handle_file(file)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', '--force',
+        action='store_true',
+        help='If set, overwrites files'
+    )
+
+    args = parser.parse_args()
+
     with Pool(cpu_count() - 3) as p:
         p.map(handle_file, files)
