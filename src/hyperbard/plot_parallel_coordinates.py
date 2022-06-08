020bb9ceadbf1cf3cec67dda791ef0ef93bd1eca
Bastian Rieck <bastian@rieck.me>
2022-06-08 12:18:56 +0200

    Added more representations for ranking

--- src/hyperbard/plot_parallel_coordinates.py
+++ src/hyperbard/plot_parallel_coordinates.py
@@ -12,14 +12,13 @@ if __name__ == '__main__':
 
     se_scene = load_graph(play, 'se-scene-w', 'n_lines')
     se_group = load_graph(play, 'se-group-w', 'n_lines')
+    se_speech = load_graph(play, 'se-speech-wd', 'n_lines')
 
-    #ce_scene_b = load_graph(play, 'ce-scene-mw')
-    #ce_scene_mb = load_graph(play, 'ce-scene-w', 'count')
-    #ce_scene_mw = load_graph(play, 'ce-scene-mw', 'n_lines')
+    ce_scene_b = load_graph(play, 'ce-scene-w', 'count')
+    ce_scene_m = load_graph(play, 'ce-scene-mw', 'n_lines')
 
-    #ce_group_b = load_graph(play, 'ce-group-mw')
-    #ce_group_mb = load_graph(play, 'ce-group-w', 'count')
-    #ce_group_mw = load_graph(play, 'ce-group-mw', 'n_lines')
+    ce_group_b = load_graph(play, 'ce-group-w')
+    ce_group_m = load_graph(play, 'ce-group-mw', 'n_lines')
 
     representations = [
         {
@@ -36,8 +35,47 @@ if __name__ == '__main__':
             'graph': se_group,
         },
         {
-            'name': '02_se-group-w',
+            'name': '04_se-group-w',
             'graph': se_group,
+            'weight': 'n_lines',
+        },
+        {
+            'name': '05_se-speech-wd_in',
+            'graph': se_speech,
+            'weight': 'n_lines',
+            'degree': 'in',
+        },
+        {
+            'name': '06_se-speech-wd_out',
+            'graph': se_speech,
+            'weight': 'n_lines',
+            'degree': 'out',
+        },
+        {
+            'name': '07_ce-scene-b',
+            'graph': ce_scene_b,
+        },
+        {
+            # TODO: Is this correct?
+            'name': '08_ce-scene-mb',
+            'graph': ce_scene_m,
+        },
+        {
+            'name': '09_ce-scene-mw',
+            'graph': ce_scene_m,
+            'weight': 'n_lines',
+        },
+        {
+            'name': '10-ce-group-b',
+            'graph': ce_group_b,
+        },
+        {
+            'name': '11-ce-group-mb',
+            'graph': ce_group_m,
+        },
+        {
+            'name': '12-ce-group-mw',
+            'graph': ce_group_m,
             'weight': 'n_lines',
         },
     ]
