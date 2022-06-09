58c100a176fef9fa1701d2ce1f1acce93716521a
dataspider <corinna.coupette@gmail.com>
2022-06-09 18:50:31 +0200

    romeo and juliet ipynb retired; fig 10 also produced by plot_romeo

--- src/hyperbard/plot_romeo.py
+++ src/hyperbard/plot_romeo.py
@@ -3,12 +3,29 @@ import matplotlib.cm as cm
 import matplotlib.patheffects as PathEffects
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 import pandas as pd
+import seaborn as sns
 from matplotlib.text import Annotation
 
 from hyperbard.graph_io import load_graph, load_hypergraph
 from hyperbard.plotting_utils import save_pgf_fig, set_rcParams
 from hyperbard.statics import GRAPHDATA_PATH, PAPERGRAPHICS_PATH
+
+
+def get_character_color(k):
+    if k == "Romeo":
+        return cm.tab10(1)
+    if k == "Juliet":
+        return cm.tab10(3)
+    if k == "Nurse":
+        return cm.tab10(2)
+    if k == "Capulet":
+        return cm.tab10(0)
+    if k == "LadyCapulet":
+        return cm.tab10(4)
+    else:
+        return "k"
 
 
 def draw_hypergraph(H, node_radius, edge_width, fontsize, tax, layout_kwargs):
@@ -652,3 +669,91 @@ if __name__ == "__main__":
             tight=True,
         )
         plt.close()
+
+    # Hypergraph over time
+    hg_speech_mwd = pd.read_csv(
+        f"{GRAPHDATA_PATH}/romeo-and-juliet_hg-speech-mwd.edges.csv"
+    )
+    hg_speech_mwd.speaker = hg_speech_mwd.speaker.map(
+        lambda x: [
+            y
+            for y in map(prettify_identifier, x.split())
+            if not y.isupper() and not y.startswith("SERVANT")
+        ]
+    )
+    hg_speech_mwd.onstage = hg_speech_mwd.onstage.map(
+        lambda x: [
+            y
+            for y in map(prettify_identifier, x.split())
+            if not y.isupper() and not y.startswith("SERVANT")
+        ]
+    )
+    hg_speech_mwd["n_lines_cumulative"] = hg_speech_mwd.n_lines.cumsum()
+    hg_speech_mwd_exploded_speaker = hg_speech_mwd.explode("speaker")
+    speaker_df = pd.DataFrame(
+        index=sorted({elem for sublist in hg_speech_mwd.speaker for elem in sublist}),
+        columns=hg_speech_mwd.n_lines_cumulative,
+    )
+    for line in hg_speech_mwd.n_lines_cumulative:
+        queried = (
+            hg_speech_mwd_exploded_speaker.query("n_lines_cumulative <= @line")
+            .groupby("speaker")
+            .agg(dict(n_lines=sum))
+        )
+        for idx, n_lines in queried.n_lines.items():
+            speaker_df.at[idx, line] = n_lines / line
+    transformed_df = speaker_df.T[
+        [x for x in speaker_df.index if x != "Chorus"]
+    ].fillna(0)
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 7))
+    ax.vlines(
+        x=[
+            hg_speech_mwd.query("act == @act").n_lines_cumulative.max()
+            for act in range(0, 5)
+        ],
+        lw=1,
+        ymin=0,
+        ymax=0.25,
+        colors="k",
+        linestyles="--",
+    )
+    transformed_df[
+        [c for c in transformed_df.columns if c not in selected_labels]
+    ].plot.line(
+        ax=ax,
+        color={k: get_character_color(k) for k in transformed_df.columns},
+        lw=1,
+        linestyle="-",
+        legend=False,
+    )
+    transformed_df[sorted(selected_labels)].plot.line(
+        ax=ax,
+        color={k: get_character_color(k) for k in transformed_df.columns},
+        linestyle="-",
+        lw=3,
+    )
+    sns.despine(offset=0, trim=True)
+    handles, labels = ax.get_legend_handles_labels()
+    plt.xticks(
+        range(0, hg_speech_mwd.n_lines_cumulative.max(), 500),
+        fontsize=font_size + 6,
+    )
+    plt.yticks(np.arange(0, 0.26, 0.05), fontsize=font_size + 6)
+    legend = ax.legend(
+        handles=handles[-5:],
+        labels=labels[-5:],
+        loc=(1.001, 0.535),
+        fontsize=font_size + 6,
+    )
+    frame = legend.get_frame()
+    frame.set_edgecolor("black")
+    frame.set_boxstyle("square", pad=0)
+    # plt.tight_layout()
+    plt.xlabel("Number of lines spoken", fontsize=font_size + 6, labelpad=9)
+    plt.ylabel("Fraction of lines spoken", fontsize=font_size + 6, labelpad=15)
+    save_pgf_fig(
+        f"{PAPERGRAPHICS_PATH}/romeo_and_juliet_hg-speech-over-time.pdf",
+        axis_off=False,
+        tight=True,
+    )
