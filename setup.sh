45537668045c1261f75e119ceec7b48a20a09e95
Bastian Rieck <bastian@rieck.me>
2022-06-09 10:50:21 +0200

    Simple setup script for creating virtual environments

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Create virtual environment or executes `poetry` installation,
+# depending on the choice of the user.
+#
+# Usage:
+#
+#   ./setup.sh poetry   # For poetry installation
+#   ./setup.sh venv     # For venv installation
+
+SETUP=${1:-poetry}
+
+if [ "${SETUP}" = poetry ]; then
+  echo "Running poetry installation"
+  poetry install
+fi
+
+if [[ "${SETUP}" = venv ]]; then
+  echo "Running venv installation"
+	python3 -m venv .venv
+	. .venv/bin/activate
+	python3 -m pip install --upgrade pip
+	pip install -r requirements.txt
+	pip install -e .
+fi
