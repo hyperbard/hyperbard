8f22f9eec8a4833c185222aecaedcbbcee360c78
dataspider <corinna.coupette@gmail.com>
2022-06-10 09:56:50 +0200

    include DATALICENSE in hyperdata.zip release

--- create_data_release.sh
+++ create_data_release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r hyperdata.zip rawdata data graphdata metadata/playtypes.csv -x "__MACOSX" -x ".DS_Store" -x "*/.DS_Store" -x "*/Makefile"
+zip -r hyperdata.zip rawdata data graphdata metadata/playtypes.csv DATALICENSE -x "__MACOSX" -x ".DS_Store" -x "*/.DS_Store" -x "*/Makefile"
